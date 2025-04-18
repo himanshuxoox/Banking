b2edb8926a5cae133a74166e27109ed0b340e97f
himanshuxoox <87160131+himanshuxoox@users.noreply.github.com>
2025-04-18 13:36:36 +0530

    login  validation flow
    Entity and repro class

---
+++ bank/src/main/java/com/Bankgos/bank/model/User.java
@@ -0,0 +1,72 @@
+package com.Bankgos.bank.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+    private  String name;
+
+    public User() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(Long id, String username, String password, String name) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
+
+
+}
---
+++ bank/src/main/java/com/Bankgos/bank/repository/UserDao.java
@@ -0,0 +1,10 @@
+package com.Bankgos.bank.repository;
+import com.Bankgos.bank.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<User,Long> {
+
+
+}
